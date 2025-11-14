aab1cba2a8681b2517a34476f885b5b29ef2c201
MDiaznf23 <muhammaddiaznurfarizki@gmail.com>
2025-11-14 21:19:50 +0700

    fix missing dependency

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ mkdir -p $BACKUP_DIR
 echo "Installing system packages..."
 sudo pacman -S --needed --noconfirm \
     i3-wm i3status i3lock polybar alacritty pcmanfm rofi picom feh scrot xclip \
-    brightnessctl playerctl imagemagick xsettingsd base-devel git \
+    brightnessctl playerctl lm_sensors imagemagick xsettingsd base-devel git \
     python python-pip python-pipx fish redshift \
     jq bc dunst
 
