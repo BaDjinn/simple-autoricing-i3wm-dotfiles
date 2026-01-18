d9fd382bcd99e8e0f98eb0b18b3af6362d1039b0
MDiaznf23 <muhammaddiaznurfarizki@gmail.com>
2026-01-18 16:25:46 +0700

    add m3-colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Restore pywal colors untuk Alacritty
-if command -v wal > /dev/null 2>&1; and test "$TERM" = "alacritty"
-   wal -Rqe
-end
-
 set fish_greeting
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -gx PATH $HOME/.local/bin $PATH
 set -gx MOZ_DISABLE_RDD_SANDBOX 1
+set -gx QT_QPA_PLATFORMTHEME qt6ct-kde
+set -gx PATH $HOME/.cargo/bin $PATH
 fish_add_path ~/.local/bin
-fish_add_path ~/.local/bin
-cat ~/.cache/wal/sequences &
+
